148ca01060f493417c59a5d13af6dfeca32abbc6
otazzu <srotazu@gmail.com>
2025-04-22 21:25:47 +0000

    Update footer page

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
 
     return (
-        <div className="bg-dark-subtle fixed-bottom" data-bs-theme="dark">
+        <div className="p-3 bg-secondary-subtle text-secondary-emphasis fixed-bottom text-center" data-bs-theme="dark">
             <p>Copy right © reserved 2025</p>
         </div>
     )
