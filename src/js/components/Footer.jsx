87a3c3f051c7f62b89cdbe6362b387c01aefe385
otazzu <srotazu@gmail.com>
2025-04-22 21:16:31 +0000

    Upadate footer page

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
 
     return (
-        <div className="bg-dark-subtle fixed-bottom">
+        <div className="bg-dark-subtle fixed-bottom" data-bs-theme="dark">
             <p>Copy right © reserved 2025</p>
         </div>
     )
