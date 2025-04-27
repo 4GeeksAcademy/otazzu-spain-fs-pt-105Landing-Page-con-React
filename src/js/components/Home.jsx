0a850d2cd252827ceb76ed003e067c4b9ce562c3
otazzu <srotazu@gmail.com>
2025-04-27 14:26:42 +0000

    Updates

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { Navbar } from "./Navbar";
+import { Card } from "./Card";
 
 const Home = () => {
 	return (
-		<Footer />
+		<>
+			<Navbar />
+			<div className="container-fluid w-75">
+				<Header />
+				<div className="justify-content-evenly">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</div>
+			</div>
+			<Footer />
+		</>
 	);
 };
 
